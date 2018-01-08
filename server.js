8190e2d54ff9cb3f9f40afa6241c48e725fe56f8
Joe Edwards <joe@gd.ly>
2018-01-08 06:36:18 -0500

    server should not be dist, we just need to move port into config

---
+++ server.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Zeus on 09/03/16.
+ */
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+var Routes = require('./routes');
+var db = require('./misc/db');
+var Inert = require('inert');
+
+server.register(Inert, function () {
+    server.connection({port: 80,routes: { cors: true }});
+    server.route(Routes.endpoints);
+    //server.timeout = 120000;
+    server.start(function(e) { console.log('Loud and clear: http://104.131.141.177') });
+});
+
+module.exports = server;
