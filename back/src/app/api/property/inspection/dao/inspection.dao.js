72a4e256e8cc2eefa1f3cbe9adbfef00f7331537
Igor Kalashnikov <zo0m.cfg@gmail.com>
2018-08-21 19:36:24 +0200

    added populated Records/DeviceType/EquipmentType fields for /devices/qrcode/{qrcode} call

--- back/src/app/api/property/inspection/dao/inspection.dao.js
+++ back/src/app/api/property/inspection/dao/inspection.dao.js
@@ -7,6 +7,10 @@ class InspectionDAO extends PropertyChildrenBaseDAO {
     constructor() {
         super(InspectionModel);
     }
+
+    getInspectionsForDeviceID(DeviceID) {
+        return this.model.find({DeviceID});
+    }
 }
 
 module.exports = new InspectionDAO(InspectionModel);
--- back/src/app/core/mail.service.js
+++ back/src/app/core/mail.service.js
@@ -1,31 +1,84 @@
 'use strict';
 
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+const logger = require('./logger');
+
+const isDebug = process.env.ENV !== 'production';
+const getMailConfig = () => !isDebug
+    ? {
+        host: 'smtp.sendgrid.net',
+        port: 2525,
+        auth: {
+            user: 'apikey',
+            pass: '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'
+        },
+    }
+    : {
+        // sent test mails here: https://ethereal.email/login
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'yihz3yz2g3dh2blk@ethereal.email',
+            pass: 'TFknB88xyDx2UBDzQF'
+        }
+    };
+
+const smtpTransport = nodemailer.createTransport(getMailConfig());
+const pooledSmtpTransport = (() => {
+    const mailConfig = getMailConfig();
+    mailConfig.pool = true;
+    return nodemailer.createTransport(mailConfig);
+})();
+
+const sendMailPromisified = function (transporter, message) {
+    return new Promise((resolve, reject) => {
+        logger.info(`Start: Send mail to ${message.to} "${message.subject}"`);
+        transporter.sendMail(message, function (error, response) {
+            if (error) {
+                logger.error(`Error: Send mail to ${message.to} "${message.subject}" failed`);
+                return reject(error);
+            } else {
+                logger.info(`Success: Send mail to ${message.to} "${message.subject}" success`);
+                return resolve(response);
+            }
+        });
+    });
+};
 
 class MailService {
-    send(to, subject, html) {
+    send(to, subject, html, attachments) {
         const name = 'FireCloud';
         const from = 'noreply_firecloud@fireprotected.com';
+        const message = {from, to, subject, html, attachments};
+        return sendMailPromisified(smtpTransport, message);
+    }
 
-        const smtpTransport = nodemailer.createTransport({
-	    host: 'smtp.sendgrid.net',
-	    port: 2525,
-	    auth: {
-		user: 'apikey',
-		pass: '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'
-            },
-        });
-
-        const mailOptions = {from, to, subject, html};
+    /**
+     * @param getNextMessages callback Array [{from, to, subject, html, attachments}, ...]
+     * @returns {Promise}
+     */
+    // pool(messages) {
+    pool(getNextMessages) {
+        let messages = [];
 
         return new Promise((resolve, reject) => {
-            smtpTransport.sendMail(mailOptions, function (error, response) {
-                if (error) {
-                    return reject(error);
-                } else {
-                    return resolve(response);
+            const flush = async() => {
+                while (pooledSmtpTransport.isIdle() && messages.length) {
+                    const message = messages.shift();
+                    sendMailPromisified(pooledSmtpTransport, message);
                 }
-            });
+
+                if (messages.length === 0) {
+                    messages = await getNextMessages();
+                    if (messages.length === 0) {
+                        resolve();
+                    } else {
+                        setTimeout(flush, 1000);
+                    }
+                }
+            };
+            pooledSmtpTransport.on('idle', flush);
+            flush();
         });
     }
 }
