e1651600bef383ada63deb76a2db02a787f25b8e
Igor Kalashnikov <zo0m.cfg@gmail.com>
2018-10-10 17:05:44 +0300

    add Image field to Map schema to store on backend

--- back/src/app/api/property/common/map/model/map.schema.js
+++ back/src/app/api/property/common/map/model/map.schema.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MapSchema = new Schema({
+    Image: {type: String},
     Scale: {type: Number},
     Left: {type: Number},
     Px: {type: Number},
