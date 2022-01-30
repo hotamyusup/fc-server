d072212548f61f7495145e300f2d1d46d4a2369b
Joe Edwards <joe@gd.ly>
2022-01-29 17:15:25 -0800

    reverted to current date/time on PDF generation

--- back/src/app/api/document/fire-safety/template/tenant-fire-safety-disclosure-information.document-builder.js
+++ back/src/app/api/document/fire-safety/template/tenant-fire-safety-disclosure-information.document-builder.js
@@ -650,8 +650,8 @@ class TenantFireSafetyDisclosureDocumentBuilder {
                                             width: "70%"
                                         },
                                         {
-                                            // text: `${moment().format(toStringDateFormat)}`,
-                                            text: `January 31, 2021`,
+                                            text: `${moment().format(toStringDateFormat)}`,
+                                            //text: `January 31, 2021`,
                                             width: "30%",
                                             alignment: "right"
 
@@ -1303,8 +1303,8 @@ class TenantFireSafetyDisclosureDocumentBuilder {
                                 width: "70%"
                             },
                             {
-                                // text: `${moment().format(toStringDateFormat)}`,
-                                text: `January 31, 2021`,
+                                text: `${moment().format(toStringDateFormat)}`,
+                                //text: `January 31, 2021`,
                                 width: "30%",
                                 alignment: "right"
 
